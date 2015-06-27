32b218896b901f815a1d4152064d2b631415bc0a
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2015-06-28 02:05:19 +0300

    rerouting commands && grep

--- out/dist/web-console.js
+++ out/dist/web-console.js
@@ -4818,9 +4818,8 @@ define('base',["require", "exports"], function (require, exports) {
             webConsole.clear();
         });
         webConsole.registerCommand("grep", function (output, args) {
-            var str = String(output).toLowerCase(), search = String(args[0]).toLowerCase(), result = "", indexes = [], i = -1;
+            var str = String(output).toLowerCase(), search = String(args[0]).toLowerCase(), result = "", i = -1;
             while ((i = str.indexOf(search, i + 1)) !== -1) {
-                // indexes.push(i);
                 result += String(output).slice(i - 100, i + 100) + "<br/>";
             }
             return result;
--- src/js/base.ts
+++ src/js/base.ts
@@ -14,11 +14,9 @@ export = function(webConsole: WebConsole) {
 		var str = String(output).toLowerCase(),
 			search = String(args[0]).toLowerCase(),
 			result: string = "",
-			indexes: number[] = [],
 			i: number = -1;
 
 		while ((i = str.indexOf(search, i + 1)) !== -1) {
-			// indexes.push(i);
 			result += String(output).slice(i - 100, i + 100) + "<br/>";
 		}
 
